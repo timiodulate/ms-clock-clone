4cfded3769bce17574bede9772f499e54c8da508
timiodulate <timiodulate@gmail.com>
2022-10-30 23:42:19 +0100

    feat: adds ms to stopwatch

--- pages/pomodoro-timer/index.tsx
+++ pages/pomodoro-timer/index.tsx
@@ -23,6 +23,7 @@ const IndexPage = () => {
 		hr: 0,
 		min: 0,
 		sec: 0,
+		ms: 0,
 	});
 
 	useEffect(() => {
@@ -109,6 +110,7 @@ const IndexPage = () => {
 					stopwatchTime={stopwatchTime}
 					startStopwatch={startStopwatch}
 					updateState={updateState}
+					setStopwatchTime={setStopwatchTime}
 				/>
 			</main>
 		</PomodoroLayout>
