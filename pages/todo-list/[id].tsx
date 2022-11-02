b92f44937d191c8d47d70965b854b30bf58f12a1
timiodulate <timiodulate@gmail.com>
2022-11-02 12:00:55 +0100

    modified:   pages/todo-list/[id].tsx

--- pages/todo-list/[id].tsx
+++ pages/todo-list/[id].tsx
@@ -42,28 +42,28 @@ const StaticPropsDetail = ({ id, errors }: Props) => {
 
 export default StaticPropsDetail;
 
-export const getStaticPaths: GetStaticPaths = async () => {
-	// Get the paths we want to pre-render based on users
-	const paths = sampleUserData.map((user) => ({
-		params: { id: user.id.toString() },
-	}));
+// export const getStaticPaths: GetStaticPaths = async () => {
+// 	// Get the paths we want to pre-render based on users
+// 	const paths = sampleUserData.map((user) => ({
+// 		params: { id: user.id.toString() },
+// 	}));
 
-	// We'll pre-render only these paths at build time.
-	// { fallback: false } means other routes should 404.
-	return { paths, fallback: true };
-};
+// 	// We'll pre-render only these paths at build time.
+// 	// { fallback: false } means other routes should 404.
+// 	return { paths, fallback: true };
+// };
 
-// This function gets called at build time on server-side.
-// It won't be called on client-side, so you can even do
-// direct database queries.
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-	try {
-		const id = params?.id;
-		// const item = sampleUserData.find((data) => data.id === Number(id));
-		// By returning { props: item }, the StaticPropsDetail component
-		// will receive `item` as a prop at build time
-		return { props: { id } };
-	} catch (err: any) {
-		return { props: { errors: err.message } };
-	}
-};
+// // This function gets called at build time on server-side.
+// // It won't be called on client-side, so you can even do
+// // direct database queries.
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+// 	try {
+// 		const id = params?.id;
+// 		// const item = sampleUserData.find((data) => data.id === Number(id));
+// 		// By returning { props: item }, the StaticPropsDetail component
+// 		// will receive `item` as a prop at build time
+// 		return { props: { id } };
+// 	} catch (err: any) {
+// 		return { props: { errors: err.message } };
+// 	}
+// };
