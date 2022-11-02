142a8284f18ac01fdfd464ae5a0ccc6504df7d44
timiodulate <timiodulate@gmail.com>
2022-11-02 12:05:32 +0100

    fix: fixes components page

--- pages/components.tsx
+++ pages/components.tsx
@@ -1,69 +1,13 @@
-import { BsFillStopCircleFill } from "react-icons/bs";
-
-const TrackFocusSection = ({
-	sessionTime,
-	updateState,
-	currentSession,
-	focusTime,
-	totalBreakMin,
-}: any) => {
+const TrackFocusSection = () => {
 	const percent = 50;
 
 	return (
-		<section className="track-focus-section">
-			<div>
-				<CircleProgress percent={percent} circleWidth={"220px"}>
-					<div>
-						{sessionTime.min.toString().length == 2
-							? sessionTime.min
-							: "0" + sessionTime.min}
-						:
-						{sessionTime.secs.toString().length == 2
-							? sessionTime.secs
-							: "0" + sessionTime.secs}
-					</div>
-				</CircleProgress>
-
-				<span
-					onClick={() => {
-						updateState("startFocus", false);
-						updateState("currentSession", "focus");
-					}}
-				>
-					<BsFillStopCircleFill />
-				</span>
-
-				<p>
-					Next:{" "}
-					<b>
-						{currentSession != "focus"
-							? `${focusTime} mins focus`
-							: `${totalBreakMin} mins break`}
-					</b>
-				</p>
-			</div>
-
-			{/* <div>
-				<div>
-					<h2>Total Time Used</h2>
-				</div>
-				<div>
-					<h2>Total Focus Time</h2>
-					<div>
-						<div>{focusMin}</div>
-						<div>{focusSecs}</div>
-					</div>
-				</div>
-				<div>
-					<h2>Total Break Time</h2>
-					<div></div>
-				</div>
-				<div>
-					<h2>Total Pauses Time</h2>
-					<div></div>
-				</div>
-			</div> */}
-		</section>
+		<main className="components-section">
+			<CircleProgress
+				percent={percent}
+				circleWidth={"220px"}
+			></CircleProgress>
+		</main>
 	);
 };
 
