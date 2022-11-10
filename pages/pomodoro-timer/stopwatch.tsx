6cb7b07a9feb0bd5fdad77b31db542613effef97
timiodulate <timiodulate@gmail.com>
2022-11-10 22:42:26 +0100

    refactor: cleans up clock/refactor page

--- pages/pomodoro-timer/stopwatch.tsx
+++ pages/pomodoro-timer/stopwatch.tsx
@@ -1,32 +1,11 @@
-import { useState } from "react";
 import { StopwatchSection } from "../../src/components/timer";
 import PomodoroLayout from "../../src/components/timer/PomodoroLayout";
 
 const IndexPage = () => {
-	const [startStopwatch, setStartStopwatch] = useState(false);
-	const [stopwatchTime, setStopwatchTime] = useState({
-		hr: 0,
-		min: 0,
-		sec: 0,
-		ms: 0,
-	});
-
-	const updatePageState = (stateTitle: any, arg: any) => {
-		if (stateTitle == "startStopwatch") {
-			setStartStopwatch(arg);
-		} else if (stateTitle == "stopwatchTime") {
-			setStopwatchTime(arg);
-		}
-	};
-
 	return (
 		<PomodoroLayout>
 			<main className="timer-page">
-				<StopwatchSection
-					updateState={updatePageState}
-					stopwatchTime={stopwatchTime}
-					startStopwatch={startStopwatch}
-				/>
+				<StopwatchSection />
 			</main>
 		</PomodoroLayout>
 	);
