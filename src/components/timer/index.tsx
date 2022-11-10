769b3a83b5391ded7f228c8a44bad131647e8551
timiodulate <timiodulate@gmail.com>
2022-11-11 00:20:06 +0100

    adds new fles

--- src/components/timer/index.tsx
+++ src/components/timer/index.tsx
@@ -2,10 +2,14 @@ import FocusTimerSection from "./FocusTimer";
 import SetFocusSection from "./SetFocus";
 import TrackFocusSection from "./TrackFocus";
 import StopwatchSection from "./Stopwatch";
+import TodoSection from "./Todo";
+import DailyProgressSection from "./DailyProgress";
 
 export {
 	FocusTimerSection,
 	SetFocusSection,
 	TrackFocusSection,
 	StopwatchSection,
+	TodoSection,
+	DailyProgressSection,
 };
