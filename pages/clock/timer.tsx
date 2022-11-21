080609f04a3a79ef6844920dcefbf10829e8326c
timiodulate <timiodulate@gmail.com>
2022-11-21 08:58:08 +0100

    fix: fixes deployment issue

--- pages/clock/timer.tsx
+++ pages/clock/timer.tsx
@@ -1,12 +1,10 @@
 import ClockLayout from "../../src/clock/layouts/ClockLayout";
-import { TrackTimerSection } from "../../src/clock/components/timer-page";
+// import { TrackTimerSection } from "../../src/clock/components/timer-page";
 
 const IndexPage = () => {
 	return (
 		<ClockLayout>
-			<main className="timer-page">
-				<TrackTimerSection />
-			</main>
+			<main className="timer-page">{/* <TrackTimerSection /> */}</main>
 		</ClockLayout>
 	);
 };
